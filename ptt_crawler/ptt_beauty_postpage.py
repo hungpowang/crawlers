624e38b597b5041cb045132161badb532c0bde39
Hungpo <muffinwang@g.ntu.edu.tw>
2020-06-29 00:32:29 +0800

    add download and zip function

--- ptt_crawler/ptt_beauty_postpage.py
+++ ptt_crawler/ptt_beauty_postpage.py
@@ -1,6 +1,9 @@
 #from urllib.request import urlopen, Request
 import requests
 from bs4 import BeautifulSoup as bs
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+from os import remove
 
 # 移除bs4的warning
 import warnings
@@ -10,11 +13,8 @@ warnings.filterwarnings('ignore')
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = "https://www.ptt.cc/bbs/Beauty/M.1592192249.A.177.html"
-# 待處理： 原po回應推文尚未移除掉
-
-#url = "https://www.ptt.cc/bbs/Gossiping/M.1592240936.A.510.html"
-#url = "https://www.ptt.cc/bbs/MuscleBeach/M.1592112509.A.BC5.html"
+# PTT beauty
+url = "https://www.ptt.cc/bbs/Beauty/M.1593154950.A.1AB.html"
 
 jar = requests.cookies.RequestsCookieJar()
 # 可把不同網頁的 cookie 設定進一個jar
@@ -39,17 +39,6 @@ right_meta = main_content.find_all("div", class_="article-metaline-right")
 for single_meta in right_meta:
     single_meta.extract()
 
-# Remove(extract) imgur圖片 ----------------------------
-# 1. 第一個部分，連結
-photo_hrefs = main_content.find_all("a")
-for pic in photo_hrefs:
-    if 'imgur' in pic["href"]:
-        pic.extract()
-# 2. 第二個部分，圖片顯示(richcontent)
-richcontents = main_content.find_all("div", class_="richcontent")
-for rich in richcontents:
-    rich.extract()
-
 # Remove(extract) 推文前   ------------------------------
 datas = main_content.find_all("span", class_="f2")
 for data in datas:
@@ -67,11 +56,48 @@ for single_push in pushes:
     single_push.extract()
 
 
+# Remove(extract) imgur圖片 ----------------------------
+# 1. 第一個部分，連結
+img_l = []
+photo_hrefs = main_content.find_all("a")
+for pic in photo_hrefs:
+    if 'imgur' in pic["href"] and 'https' in pic["href"]:
+        img_l.append(pic["href"])
+        pic.extract()
+## 2. 第二個部分，圖片顯示(richcontent)
+#richcontents = main_content.find_all("div", class_="richcontent")
+#for rich in richcontents:
+#    rich.extract()
+
 
 
 for (m, v) in zip(metas, m_values):
     print(m.text, ':', v.text)
 
 print("分數 :", score)
+print("內文 :")
 
-print(main_content.text)
+#print(main_content.text)
+content = main_content.text
+#content_split = content.split('--')
+origin_content = content.split('--')[0]
+ori_l = origin_content.split("\n")
+ori_linted = []
+for o in ori_l:
+    if o != "":
+        ori_linted.append(o)
+for i in ori_linted:
+    print(i)
+
+print("圖片連結 :")
+for img in img_l:
+    ## 下載圖片
+    #urlretrieve(img, img.split('/')[-1])
+    print(img)
+
+## 壓成zip檔，以推文數+照片張數命名，壓縮完成後刪除圖片
+#with ZipFile('PTTBeauty_'+str(score)+'_'+str(len(img_l))+'.zip', 'w') as myzip:
+#    for img in img_l:
+#        filename = img.split('/')[-1]
+#        myzip.write(filename)
+#        remove(filename)
